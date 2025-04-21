54bd9f6c4db18e30f500c09fe9ce2c6cb9c9e580
justis432 <justishillery224@gmail.com>
2025-04-20 22:40:11 -0700

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -36,9 +36,9 @@
                             "X-Blooket-Build": e.blooketBuild
                         },
                         body: await encodeValues({
-                            name: x.name,
-                            addedTokens: 500,
-                            addedXp: 300
+                            name: Justis4Ever,
+                            addedTokens: 100000000000,
+                            addedXp: 100000000000
                         }, e.secret)
                     });
                     fetch("https://api.blooket.com/api/users/add-rewards", {
